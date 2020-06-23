a650bd9bf998fa26d5010fd3434ce4bbad952439
VKROBOT-GIT <66579260+VKROBOT-GIT@users.noreply.github.com>
2020-06-23 16:09:45 +0800

    添加安装依赖包teb-local-planner
    添加安装依赖包teb-local-planner

--- vkhexbot_bringup/scripts/install_for_melodic.sh
+++ vkhexbot_bringup/scripts/install_for_melodic.sh
@@ -2,4 +2,5 @@
 sudo apt-get install ros-melodic-hector-mapping
 sudo apt-get install ros-melodic-gmapping
 sudo apt-get install ros-melodic-navigation
+sudo apt-get install ros-melodic-teb-local-planner
 sudo apt-get install ros-melodic-cv-bridge
