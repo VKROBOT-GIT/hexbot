557d1acf2b08ed0243284364fb0ae9c500bbfce2
VKROBOT-GIT <vk@vk-robot.com>
2020-06-23 16:15:09 +0800

    删除多余文件夹vkhexbott_bringup/script2

--- vkhexbot_bringup/scripts2/install_for_kinetic.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt-get install ros-kinetic-hector-mapping
-sudo apt-get install ros-kinetic-gmapping
-sudo apt-get install ros-kinetic-navigation
-sudo apt-get install ros-kinetic-cv-bridge
--- vkhexbot_bringup/scripts2/install_for_melodic.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt-get install ros-melodic-hector-mapping
-sudo apt-get install ros-melodic-gmapping
-sudo apt-get install ros-melodic-navigation
-sudo apt-get install ros-melodic-cv-bridge
